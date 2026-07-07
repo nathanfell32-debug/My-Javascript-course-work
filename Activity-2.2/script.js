2e05978c58c14760132e19202575e69b1519a51b
nathanfell32-debug <nathan.fell32@gmail.com>
2026-07-07 12:19:39 +0100

    Add files via upload
    Activity 2.2 - changing HTML with JavaScript.
    In my html i created a button input, and in my JavaScript file i targeted that button to change the heading when the button is clicked

---
+++ Activity-2.2/script.js
@@ -0,0 +1,4 @@
+function changeHeading() {
+    const heading = document.getElementById("mainHeading");  //Targets my h1
+    heading.textContent = "The heading has now changed!";  // replaces my text in h1
+}
