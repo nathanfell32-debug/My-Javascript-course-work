2232af7bccb5a112c14c272bf6f2c8145ebb0e29
nathanfell32-debug <nathan.fell32@gmail.com>
2026-07-29 15:35:39 +0100

    Add files via upload
    finished javascript section and styled the page with css

--- Activity-6.1/script.js
+++ Activity-6.1/script.js
@@ -4,11 +4,47 @@ let array = [];
 // function to display array
 function displayArray() {
    const arrayContainer = document.getElementById('arrayElements');
-   arrayContainer.innetHTML = '';  // clear previous content
+   arrayContainer.innerHTML = '';  // clear previous content
    
    array.forEach((element, index) => {
     const elementDiv = document.createElement('div');
-    elementDiv.textContent = 'element ${index + 1}: ${element}';
+    elementDiv.textContent = `element ${index + 1}: ${element}`;
     arrayContainer.appendChild(elementDiv);
    });
 }
+
+// function to add element to the end of the array
+function addElement() {
+    const elementInput = document.getElementById('elementInput').value.trim();
+    if (elementInput !== '') {
+        array.push(elementInput);
+        displayArray();
+    } else {
+        alert('Please enter a valid element.');
+    }
+}
+
+ // function to remove element from the beginning or end of the array
+ function removeElement(type) {
+    if (array.length === 0) {     // prevents removing from an empty array
+        alert('Array is empty.');
+        return;
+    }
+
+
+    // decides which removal method to use
+    switch (type) {
+        case 'first':
+        array.shift();   //removes first element
+        break;
+        case 'last':
+        array.pop();   //removes last element
+        break;
+        default:
+            alert('Invalid operation.');
+    }
+
+
+    // re-render the updated array
+    displayArray();
+    }
